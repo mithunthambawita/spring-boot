c0dc2fe5338ecdb93151496af4115ddad93420b8
mithun thambawita <mithunthambavita@gmail.com>
2023-05-01 23:52:48 +0530

    find one by id

--- src/AppInitializer.java
+++ src/AppInitializer.java
@@ -1,45 +1,35 @@
-import entity.Book;
 import entity.Student;
 import org.hibernate.Session;
-import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
-import org.hibernate.boot.Metadata;
-import org.hibernate.boot.MetadataSources;
-import org.hibernate.boot.registry.StandardServiceRegistry;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.hibernate.cfg.Environment;
-
-import java.io.Serializable;
-import java.util.HashMap;
-import java.util.Map;
 
 public class AppInitializer {
     public static void main(String[] args) {
-       Student student = new Student(1,"Kamal Bandara",90.00);
+       Student student = new Student(2,"Mithun Bandara",90.00);
        saveStudent(student);
-       Book book = new Book(1,"my Book");
-       saveBook(book);
+//        findStudent(1);
     }
     private static void saveStudent(Student student){
         try(Session session = HibernateUtil.getSessionFactory().openSession()) {
             //save (it return a serializable object, return primary key of the saved object)
-            //persist,saveOrUpdate
+            //persist (no return type)
+            // saveOrUpdate
             Transaction transaction = session.beginTransaction();//save, update and delete
-            long primaryKey = (Long) session.save(student);
+            session.save(student);
             transaction.commit();
-            System.out.println(primaryKey);
+
+        }
+    }
+    private static void findStudent(long id){
+        try(Session session = HibernateUtil.getSessionFactory().openSession()) {
+            Student student = session.find(Student.class,id);
+            if(student!=null){
+                System.out.println(student.toString());
+            }else {
+                System.out.println("Can not find data");
+            }
         }
     }
 
-    private static void saveBook(Book book){
-        try(Session session = HibernateUtil.getSessionFactory().openSession()) {
-            //save (it return a serializable object, return primary key of the saved object)
-            //persist,saveOrUpdate
-            Transaction transaction = session.beginTransaction();//save, update and delete
-            long primaryKey = (Long) session.save(book);
-            transaction.commit();
-            System.out.println(primaryKey);
-        }
-    }
+
 
 }
